49d3f8f6e1c63bd98b8ee5bae12d101a2a8c783d
Abraiz01 <78402495+Abraiz01@users.noreply.github.com>
2023-05-02 10:00:39 +0400

    commented app.js

--- app.js
+++ app.js
@@ -2,47 +2,47 @@
 let height = window.innerHeight;
 let width = window.innerWidth;
 
+// checking if window is at the photos.html page to load the slideshow elements and functions
 if (window.location.pathname == "/photos.html") {
-let slideIndex = 1;
-// if (window.location.pathname == "/photos.html") {
+  // the following has been adapted from https://www.w3schools.com/howto/howto_js_slideshow.asp
+  let slideIndex = 1;
   showSlides(slideIndex);
-// }
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides((slideIndex = n));
+  }
+
+  // function to display the slides
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+  }
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+// initializing buttons on the homescreen for gallery, video, and creators that
+// will take the user to the respective pages when clicked
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
-}
-
-// initializing buttons on the homescreen for story, video, and cast that
-// will scroll to the respective pages when clicked
-
-// if (window.location.pathname == "/index.html") {
 let button1;
 button1 = document
   .getElementById("button1")
@@ -82,7 +82,7 @@ if (window.location.pathname == "/index.html") {
     }
   }
 
-  // upon clicking the back-to-top button, the website scrolls back to the homepage
+  // upon clicking the back-to-top button, the website scrolls back to the top of the homepage
   // it takes an invisible placeholder at the top-left of the homepage as its reference of where to scroll to
   document.getElementById("back-to-top").addEventListener("click", function () {
     const target = document.querySelector("#placeholder-2");
